4da6833c8363e7a94da95ad949abf82427b3c4d8
Eduardo <eduardooliveira256@gmail.com>
2025-11-21 09:04:26 -0300

    middleware

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 #[allow(dead_code, unused)]
 use axum::{
-    Json, Router,
+    Json, RequestPartsExt, Router,
     extract::{FromRef, FromRequestParts, State},
-    http::{StatusCode, request::Parts},
+    http::{StatusCode, header, request::Parts},
+    response::{IntoResponse, Response},
     routing::{get, post},
 };
-use jsonwebtoken::{EncodingKey, Header, encode};
+use chrono::{Duration as ChronoDuration, Utc};
+use jsonwebtoken::{DecodingKey, EncodingKey, Header, Validation, decode, encode};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use sqlx::postgres::{PgPool, PgPoolOptions};
 use std::sync::LazyLock;
 use std::time::Duration;
@@ -15,21 +18,20 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 struct Keys {
     encoding: EncodingKey,
+    decoding: DecodingKey,
 }
 
 impl Keys {
     fn new(secret: &[u8]) -> Self {
         Self {
             encoding: EncodingKey::from_secret(secret),
+            decoding: DecodingKey::from_secret(secret),
         }
     }
 }
 
 static KEYS: LazyLock<Keys> = LazyLock::new(|| {
-    dotenvy::dotenv().ok();
-    
-    let secret = std::env::var("JWT_SECRET").unwrap_or_else(|_| "JWT_SECRET".to_string());
-
+    let secret = std::env::var("JWT_SECRET").expect("JWT_SECRET is not set");
     Keys::new(secret.as_bytes())
 });
 
@@ -60,14 +62,73 @@ struct UserResponse {
     updated_at: Option<chrono::NaiveDateTime>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct Claims {
     sub: i32,
     exp: usize,
 }
 
+#[derive(Debug)]
+enum AuthError {
+    InvalidToken,
+}
+
+impl IntoResponse for AuthError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            AuthError::InvalidToken => (StatusCode::BAD_REQUEST, "Invalid token"),
+        };
+
+        let body = Json(json!({
+            "error": error_message,
+        }));
+
+        (status, body).into_response()
+    }
+}
+
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+}
+
+impl<S> FromRequestParts<S> for Claims
+where
+    S: Send + Sync,
+{
+    type Rejection = AuthError;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        // Extract the token from the authorization header
+        let auth = parts
+            .headers
+            .get(header::AUTHORIZATION)
+            .and_then(|v| v.to_str().ok());
+
+        let Some(auth) = auth else {
+            return Err(AuthError::InvalidToken);
+        };
+
+        if !auth.starts_with("Bearer ") {
+            return Err(AuthError::InvalidToken);
+        }
+
+        let token = &auth[7..];
+
+        // Decode the user data
+        let token_data = decode::<Claims>(token, &KEYS.decoding, &Validation::default())
+            .map_err(|_| AuthError::InvalidToken)?;
+
+        Ok(token_data.claims)
+    }
+}
+
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
+
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -99,15 +160,9 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-fn internal_error<E>(err: E) -> (StatusCode, String)
-where
-    E: std::error::Error,
-{
-    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
-}
-
 async fn get_users(
     State(pool): State<PgPool>,
+    _claims: Claims,
 ) -> Result<Json<Vec<UserResponse>>, (StatusCode, String)> {
     let users = sqlx::query_as!(
         UserResponse,
@@ -186,7 +241,7 @@ async fn authenticate_user(
             &Header::default(),
             &Claims {
                 sub: user.id,
-                exp: 86400,
+                exp: (Utc::now() + ChronoDuration::hours(24)).timestamp() as usize,
             },
             &KEYS.encoding,
         )
